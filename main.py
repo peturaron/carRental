abf1e72b230371eba52c4f038a41113fa5b52518
Elín Hrund Búadóttir <ElinBua@Gujons-MacBook-Pro.local>
2018-12-06 22:59:26 +0000

    Setti inn main menu

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 from ui.SalesmanUi import SalesmanUi
 
 def main():
-    ui = SalesmanUi()
-    ui.main_menu()
+        print("Welcome to Nordic Car Rental! \nPlease enter the number for following actions:\n")
+        action = ""
+        while (action != "4"):
+            print()
+            print("1. Bookings")
+            print("2. Car")
+            print("3. Customers")
+            print("4. Quit")
 
+            action = input("Choose an option: ")
+            
+            if action == "1":
+                pass
+            elif action == "2":
+                pass
+            elif action == "3":
+                ui = SalesmanUi()
+                ui.main_menu()
+            elif action == "4":
+                break
+            else:
+                print("Please enter valid number")
+                print()
+    
 main()
