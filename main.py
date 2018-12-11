26b14ee72bd44853c6c28b9ec58f33732ed0d7ab
Asta Gisladottir <astag1@astriki.is>
2018-12-11 01:24:32 +0000

    nýjasta útgafan af main.py

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 from ui.CustomerUi import CustomerUi
 from ui.CarUi import CarUi
+from ui.BookingUi import BookingUi
 
 def main():
-        print("\n\nWelcome to Nordic Car Rental! \nPlease enter the number for following actions:\n")
+        print("Welcome to Nordic Car Rental! \nPlease enter the number for following actions:\n")
         action = ""
         while (action != "4"):
-            print("\nMAIN MENU")
+            print("\nMAIN MENU") 
             print("_"*40,"\n")
             mainMenu = "\t{:<30}\n\t{:<30}\n\t{:<30}\n\t{:<30}\n\t".format("1 - Bookings", "2 - Car", "3 - Customers", "4 - Quit")
             print(mainMenu)
 
             action = input("Choose an option: ")
-
+            
             if action == "1":
-                pass
+                bookingUi = BookingUi()
+                bookingUi.mainMenu()
             elif action == "2":
                 carUi = CarUi()
                 carUi.mainMenu()
@@ -23,7 +25,7 @@ def main():
             elif action == "4":
                 break
             else:
-                print("Please enter valid number")
+                print("Please enter valid number: ")
                 print()
-
+    
 main()
